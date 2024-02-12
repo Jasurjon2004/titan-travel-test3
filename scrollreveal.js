ee203f2e090d58de01cba656256d2821c6b844f4
Jasurjon2004 <148596755+Jasurjon2004@users.noreply.github.com>
2024-02-12 17:25:46 +0700

    Add files via upload

---
+++ scrollreveal.js
@@ -0,0 +1,12 @@
+// ScrollReveal().reveal('.box-container', {
+//   duration: 2000,
+//   easing: 'ease-in',
+//   // Add other global options here
+// });
+
+
+
+// ScrollReveal().reveal('.box' {
+//   distance: '1000px',
+//   opacity: 0.8
+// });
